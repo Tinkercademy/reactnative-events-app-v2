9fba133e91a8bc3bfcae3ca40419b33549c3c5b9
YJ Soon <yjsoon@tinkertanker.com>
2019-08-23 09:59:53 +0800

    Update header and title colour for Home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 import { Container, Content, H1 } from "native-base";
+import Colors from "../constants/Colors";
 
 export default function HomeScreen() {
   return (
@@ -14,11 +15,16 @@ export default function HomeScreen() {
 
 HomeScreen.navigationOptions = {
   title: "Home",
+  headerStyle: {
+    backgroundColor: Colors.tintColor,
+  },
+  headerTintColor: "white",
 };
 
 const styles = StyleSheet.create({
   headerText: {
     fontWeight: "bold",
     textAlign: "center",
+    color: Colors.tintColor,
   },
 });
