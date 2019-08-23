5bfa8811f339554ff02d7b28a3275fa9fbb03a40
YJ Soon <yjsoon@tinkertanker.com>
2019-08-23 11:33:52 +0800

    Added Card with info

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,14 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import { Container, Content, H1 } from "native-base";
+import { StyleSheet, Image } from "react-native";
+import {
+  Container,
+  Content,
+  Card,
+  CardItem,
+  Body,
+  Text,
+  H1,
+} from "native-base";
 import Colors from "../constants/Colors";
 
 export default function HomeScreen() {
@@ -8,6 +16,26 @@ export default function HomeScreen() {
     <Container>
       <Content padder>
         <H1 style={styles.headerText}>Basket Throwing Tournament 2019</H1>
+        <Card style={styles.card}>
+          <CardItem>
+            <Body>
+              <Text style={styles.title}>Welcome</Text>
+              <Text>
+                Thanks for downloading our app! Here, you'll find all kinds of
+                information about our upcoming event. It'll be great!
+              </Text>
+            </Body>
+          </CardItem>
+          <CardItem cardBody>
+            <Image
+              source={{
+                uri:
+                  "https://www.containerstore.com/catalogimages/339352/10074096-small-seagrass-belly-basket.jpg",
+              }}
+              style={{ height: 200, width: null, flex: 1 }}
+            />
+          </CardItem>
+        </Card>
       </Content>
     </Container>
   );
@@ -26,5 +54,15 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     color: Colors.tintColor,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  card: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
   },
 });
